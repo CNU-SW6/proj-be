54f078d844fb6b3307cfa4141bca4c465989f295
happybae <qo98333@naver.com>
2023-01-10 16:26:12 +0900

    project initialization

---
+++ src/main/java/cnu/swabe/SwabeApplication.java
@@ -0,0 +1,13 @@
+package cnu.swabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwabeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwabeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cnu/swabe/SwabeApplicationTests.java
@@ -0,0 +1,13 @@
+package cnu.swabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwabeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

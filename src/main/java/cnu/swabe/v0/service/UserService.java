b9065120752b9f9c13ebe6ca246cf62120e73ad9
happybae <qo98333@naver.com>
2023-01-27 11:07:44 +0900

    feat: DuplicateNickname exception

--- src/main/java/cnu/swabe/v0/service/UserService.java
+++ src/main/java/cnu/swabe/v0/service/UserService.java
@@ -2,6 +2,7 @@ package cnu.swabe.v0.service;
 
 import cnu.swabe.v0.domain.User;
 import cnu.swabe.v0.dto.UserDTO;
+import cnu.swabe.v0.exception.custom.NicknameDuplicatedException;
 import cnu.swabe.v0.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,19 +25,20 @@ public class UserService {
         return addedUserNo;
     }
 
-    /**
-     * 여기서도 중복체크를 해야할까?
-     * 비즈니스 로직이 필요할까?
-     * */
-    public boolean checkDuplicateNickName(String nickname) {
-        boolean isExist = userRepository.findByNickName(nickname);
-        return isExist;
+    public User checkDuplicateNickName(String nickname) {
+        User user = userRepository.findByNickName(nickname);
+        if(user == null) {
+            throw new NicknameDuplicatedException();
+        }
+
+        return user;
     }
 
     public User login(UserDTO userDTO) {
         User userbyId = userRepository.findById(userDTO.getId());
         User userbyPw = userRepository.findByPw(userDTO.getPw());
 
+        // 객체 비교 이렇게 노노
         if (userbyId == userbyPw) {
             return userbyPw;
         }

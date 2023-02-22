382d42076c1d032ddfceb1231f505044e6509321
hyunjunKWON <bonheur9813@naver.com>
2023-02-22 14:45:33 +0900

    feat: Add Config

---
+++ src/main/java/cnu/swabe/v2/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package cnu.swabe.v2.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll();
+        return httpSecurity.build();
+    }
+}
---
+++ src/main/java/cnu/swabe/v2/config/WebConfig.java
@@ -0,0 +1,14 @@
+package cnu.swabe.v2.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://10.188.191.209:8080")
+                .allowedMethods("*");
+    }
+}
